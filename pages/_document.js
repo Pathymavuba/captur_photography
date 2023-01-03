34789d0cfb42f3575aa53416f754aad9124ad4c0
pathymavuba <mavubapathy@gmail.com>
2023-01-03 14:26:55 +0100

    navbar done

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,14 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Link from 'next/link'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;1,300;1,400;1,600&family=Lexend+Deca:wght@400;500;600;700&family=Raleway:wght@100;200;300;400;500;600;700;800&display=swap" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
