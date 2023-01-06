06e98d673329ef4c616fbc3198dd7802481c8a49
pathymavuba <mavubapathy@gmail.com>
2023-01-06 11:16:02 +0100

    review Hero component

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = ({heading,message}) => {
   return (
     <div className='flex items-center justify-center h-screen mb-12 bg-center bg-cover custom-image'>
         {/* overlay */}
-        <div className='absolute top-0 right-0 left-0 bottom-0 bg-black/70 z-[2]' />
+        <div className='absolute top-0 right-0 left-0 bottom-0 bg-black/70 z-[2] h-full' />
 
         <div className='p-5 text-white z-[2] mt-[-10rem]' >
         <h1 className='text-5xl font-bold'>{heading}</h1>
